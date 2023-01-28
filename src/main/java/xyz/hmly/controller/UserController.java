e54a1dfd51186e8abbd9a1e305b7b198f6b70657
hlmy <yodc887@gmail>
2023-01-28 11:42:10 +0800

    Initial commit

---
+++ src/main/java/xyz/hmly/controller/UserController.java
@@ -0,0 +1,8 @@
+package xyz.hmly.controller;/**
+ * @ProjectName:    ehcache 
+ * @ClassName:      UserController
+ * @Author:     lipenghui
+ * @Description:    
+ * @Date:    2023/01/28 11:21
+ */public class UserController {
+}
